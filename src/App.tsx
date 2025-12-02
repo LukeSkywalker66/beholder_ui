4f2494c0093bfc3ff638b3dfc44d6338088f65f4
LukeSkywalker66 <lucascaceres@gmail.com>
2025-12-02 00:05:05 -0300

    segundo cambio2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+
 import SearchBox from "./components/SearchBox"
-// import CopyButton from "./CopyButton";
+
 
 
 import './App.css'
