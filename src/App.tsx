e8bc3936518add7fae22030b3e6c90a160db3b0c
LukeSkywalker66 <lucascaceres@gmail.com>
2025-12-02 10:51:09 -0300

    Primer prueba en producción5

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
+import SearchBox from "./components/SearchBox";
+import './App.css';
 
-import SearchBox from "./components/SearchBox"
-
-
-
-import './App.css'
+// Importá la imagen desde assets
+import portadaImg from './assets/beholder1.png';
 
 function App() {
   return (
-    <div>
-      <h1>Beholder</h1>
-      <h2>Diagnóstico centralizado de 2F Internet</h2>
-      <SearchBox />
+    <div className="app">
+      <header className="header">
+        <h1>Beholder</h1>
+        <h2>Diagnóstico centralizado de 2F Internet</h2>
+      </header>
+
+      {/* Imagen de portada */}
+      <div className="portada">
+        <img src={portadaImg} alt="Portada Beholder" className="portada-img" />
+      </div>
+
+      <main>
+        <SearchBox />
+      </main>
     </div>
   );
 }
 
 export default App;
-
