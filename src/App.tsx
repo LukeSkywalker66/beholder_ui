83f885a014f2577cda4f5bd5659ef2e5605c993a
LukeSkywalker66 <lucascaceres@gmail.com>
2025-12-01 23:58:06 -0300

    primer commit desde casa

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import SearchBox from "./components/SearchBox"
 // import CopyButton from "./CopyButton";
 
