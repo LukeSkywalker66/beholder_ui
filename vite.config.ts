8714c4859da1c739fbecb2f1712ce415b66dbf60
LukeSkywalker66 <lucascaceres@gmail.com>
2025-12-24 12:37:29 -0300

    finalizando dockerizacion de beholder2

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  base: '/monitor/',
   server: {
-    proxy: {
-      '/api': 'http://localhost:8500'
-    }
+    host: true, // Para que Docker lo vea
+    port: 5173  // Aseguramos el puerto
   }
 })
